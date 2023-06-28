010690191f6235c0d7a37b26acf87619b8fd7ce1
Victor Wen <vicwen02@g.ucla.edu>
2023-06-28 16:36:27 -0400

    Added new effects

--- src/effects.cpp
+++ src/effects.cpp
@@ -68,3 +68,168 @@ vec4 SlideEffect::draw(float index, const ChromaState& state) const {
     index = fmod(1 + index - offset, 1);
     return this->effect->draw(index, state);
 }
+
+WipeEffect::WipeEffect(const std::vector<ChromaData>& args) : ChromaEffect("slide") {
+    this->effect = args[0].get_effect();
+    this->time = args[1].get_float();
+    this->start = -1;
+}
+
+void WipeEffect::tick(const ChromaState& state) {
+    if (start < 0) this->start = state.time;
+    this->effect->tick(state);
+}
+
+vec4 WipeEffect::draw(float index, const ChromaState& state) const {
+    float offset = fmod(state.get_time_diff(start) / this->time, 1);
+    return this->effect->draw(offset, state);
+}
+
+BlinkEffect::BlinkEffect(const std::vector<ChromaData> &args)
+{
+    this->effect = args[0].get_effect();
+    this->time = args[1].get_float();
+    this->start = -1;
+}
+
+void BlinkEffect::tick(const ChromaState& state) {
+    if (start < 0) this->start = state.time;
+    this->effect->tick(state);
+
+    int i = floor(state.get_time_diff(this->start) / this->time);
+    this->on = i % 2 == 0;
+}
+
+vec4 BlinkEffect::draw(float index, const ChromaState& state) const {
+    vec4 color = this->effect->draw(index, state);
+    if (this->on)
+        return color;
+    else
+        return vec4(0, 0, 0, 0); // TODO: decide to use premultiplied or straight alpha
+}
+
+BlinkFadeEffect::BlinkFadeEffect(const std::vector<ChromaData> &args)
+{
+    this->effect = args[0].get_effect();
+    this->time = args[1].get_float();
+    this->start = -1;
+}
+
+void BlinkFadeEffect::tick(const ChromaState& state) {
+    if (start < 0) this->start = state.time;
+    this->effect->tick(state);
+
+    float t = fmod(state.get_time_diff(this->start) / this->time, 2);
+    this->transition = 1.0 - abs(t - 1.0);
+}
+
+vec4 BlinkFadeEffect::draw(float index, const ChromaState& state) const {
+    vec4 color = this->effect->draw(index, state);
+    return color * this->transition;
+}
+
+WormEffect::WormEffect(const std::vector<ChromaData> &args)
+{
+    this->effect = args[0].get_effect();
+    this->time = args[1].get_float();
+    this->start = -1;
+}
+
+void WormEffect::tick(const ChromaState& state) {
+    if (start < 0) this->start = state.time;
+    this->effect->tick(state);
+
+    this->cutoff = state.get_time_diff(this->start) / this->time;
+}
+
+vec4 WormEffect::draw(float index, const ChromaState& state) const {
+    if (index <= this->cutoff)
+        return this->effect->draw(index, state);
+    else
+        return vec4(0, 0, 0, 0);
+}
+
+FadeInEffect::FadeInEffect(const std::vector<ChromaData> &args)
+{
+    this->effect = args[0].get_effect();
+    this->time = args[1].get_float();
+    this->start = -1;
+}
+
+void FadeInEffect::tick(const ChromaState& state) {
+    if (start < 0) this->start = state.time;
+    this->effect->tick(state);
+
+    float t = state.get_time_diff(this->start) / this->time;
+    if (t > 1)
+        t = 1;
+    this->transition = t;
+}
+
+vec4 FadeInEffect::draw(float index, const ChromaState& state) const {
+    vec4 color = this->effect->draw(index, state);
+    return color * this->transition;
+}
+
+FadeOutEffect::FadeOutEffect(const std::vector<ChromaData> &args)
+{
+    this->effect = args[0].get_effect();
+    this->time = args[1].get_float();
+    this->start = -1;
+}
+
+void FadeOutEffect::tick(const ChromaState& state) {
+    if (start < 0) this->start = state.time;
+    this->effect->tick(state);
+
+    float t = 1.0 - state.get_time_diff(this->start) / this->time;
+    if (t < 0)
+        t = 0;
+    this->transition = t;
+}
+
+vec4 FadeOutEffect::draw(float index, const ChromaState& state) const {
+    vec4 color = this->effect->draw(index, state);
+    return color * this->transition;
+}
+
+WaveEffect::WaveEffect(const std::vector<ChromaData> &args)
+{
+    this->effect = args[0].get_effect();
+    this->period = args[1].get_float();
+    this->wavelength = args[2].get_float();
+
+    this->start = -1;
+}
+
+void WaveEffect::tick(const ChromaState& state) {
+    if (start < 0) this->start = state.time;
+    this->effect->tick(state);
+}
+
+vec4 WaveEffect::draw(float index, const ChromaState& state) const {
+    float t = state.get_time_diff(this->start);
+    float phase = (index / this->wavelength - t / this->period) * 2 * M_PI;
+    float val = (1 + sin(phase)) / 2;
+    return this->effect->draw(val, state);
+}
+
+WheelEffect::WheelEffect(const std::vector<ChromaData> &args)
+{
+    this->effect = args[0].get_effect();
+    this->period = args[1].get_float();
+
+    this->start = -1;
+}
+
+void WheelEffect::tick(const ChromaState& state) {
+    if (start < 0) this->start = state.time;
+    this->effect->tick(state);
+}
+
+vec4 WheelEffect::draw(float index, const ChromaState& state) const {
+    float t = state.get_time_diff(this->start);
+    float phase = (t / this->period) * 2 * M_PI;
+    float val = (1 + sin(phase)) / 2;
+    return this->effect->draw(val, state);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,35 @@ const auto SLIDE_CMD = CommandBuilder<SlideEffect>("slide")
     .add_argument("EFFECT", OBJECT_TYPE, "effect to slide around the strip") //TODO: have an effect verifier
     .add_argument("TIME", NUMBER_TYPE, "time to finish a loop in seconds")
     .set_description("Slides an effect with looping");
-
+const auto WIPE_CMD = CommandBuilder<WipeEffect>("wipe")
+    .add_argument("EFFECT", OBJECT_TYPE, "effect to wipe around the strip")
+    .add_argument("TIME", NUMBER_TYPE, "time to finish a wipe in seconds")
+    .set_description("Wipes an effect with looping");
+const auto BLINK_CMD = CommandBuilder<BlinkEffect>("blink")
+    .add_argument("EFFECT", OBJECT_TYPE, "effect to blink")
+    .add_argument("TIME", NUMBER_TYPE, "time between blinking on and off")
+    .set_description("Blinks an effect on and off");
+const auto BLINKFADE_CMD = CommandBuilder<BlinkFadeEffect>("blinkfade")
+    .add_argument("EFFECT", OBJECT_TYPE, "effect to fade")
+    .add_argument("TIME", NUMBER_TYPE, "time between fading on and off")
+    .set_description("Fades an effect on and off");
+const auto FADEIN_CMD = CommandBuilder<FadeInEffect>("fadein")
+    .add_argument("EFFECT", OBJECT_TYPE, "effect to fade")
+    .add_argument("TIME", NUMBER_TYPE, "time from off to on")
+    .set_description("Fades an effect from off to on");
+const auto FADEOUT_CMD = CommandBuilder<FadeOutEffect>("fadeout")
+    .add_argument("EFFECT", OBJECT_TYPE, "effect to fade")
+    .add_argument("TIME", NUMBER_TYPE, "time from on to off")
+    .set_description("Fades an effect from on to off");
+const auto WAVE_CMD = CommandBuilder<WaveEffect>("wave")
+    .add_argument("EFFECT", OBJECT_TYPE, "effect to wave")
+    .add_argument("PERIOD", NUMBER_TYPE, "period of the wave")
+    .add_argument("WAVELENGTH", NUMBER_TYPE, "wavelength (in pixels) of the wave")
+    .set_description("Moves the effect across the strip with a wave distribution");
+const auto WHEEL_CMD = CommandBuilder<WheelEffect>("wheel")
+    .add_argument("EFFECT", OBJECT_TYPE, "effect to spin")
+    .add_argument("PERIOD", NUMBER_TYPE, "period of the spin")
+    .set_description("Spins the effect across the strip like a wheel (wave-like version of wipe)");
 
 void callback(const std::vector<vec4>& pixels) {
     const int n = 150;
@@ -120,6 +148,28 @@ int main() {
     fprintf(stderr, "%s\n", SLIDE_CMD.get_help().c_str());
     cenv.functions["slide"] = std::make_unique<CommandBuilder<SlideEffect>>(SLIDE_CMD);
 
+    fprintf(stderr, "%s\n", WIPE_CMD.get_help().c_str());
+    cenv.functions["wipe"] = std::make_unique<CommandBuilder<WipeEffect>>(WIPE_CMD);
+
+    fprintf(stderr, "%s\n", BLINK_CMD.get_help().c_str());
+    cenv.functions["blink"] = std::make_unique<CommandBuilder<BlinkEffect>>(BLINK_CMD);
+
+    fprintf(stderr, "%s\n", BLINKFADE_CMD.get_help().c_str());
+    cenv.functions["blinkfade"] = std::make_unique<CommandBuilder<BlinkFadeEffect>>(BLINKFADE_CMD);
+
+    fprintf(stderr, "%s\n", FADEIN_CMD.get_help().c_str());
+    cenv.functions["fadein"] = std::make_unique<CommandBuilder<FadeInEffect>>(FADEIN_CMD);
+
+    fprintf(stderr, "%s\n", FADEOUT_CMD.get_help().c_str());
+    cenv.functions["fadeout"] = std::make_unique<CommandBuilder<FadeOutEffect>>(FADEOUT_CMD);
+
+    fprintf(stderr, "%s\n", WAVE_CMD.get_help().c_str());
+    cenv.functions["wave"] = std::make_unique<CommandBuilder<WaveEffect>>(WAVE_CMD);
+
+    fprintf(stderr, "%s\n", WHEEL_CMD.get_help().c_str());
+    cenv.functions["wheel"] = std::make_unique<CommandBuilder<WheelEffect>>(WHEEL_CMD);
+    
+
     // process_input("let r = rainbow", cenv, true);
     // process_input("let ten = 10", cenv, true);
     // process_input("func slide10 x = slide x ten", cenv, true);
@@ -128,7 +178,18 @@ int main() {
     process_input("let BLUE = rgb 0 0 255", cenv, true);
     // process_input("let p = split RED (gradient RED BLUE)", cenv, true);
     // process_input("gradient (slide10 p) (slide10 r)", cenv, true);
-    process_input("split RED BLUE GREEN", cenv, true);
+
+
+    process_input("let r = gradient RED GREEN BLUE", cenv, true);
+    process_input("let a = wipe r 10", cenv, true);
+    process_input("let b = blink r 10", cenv, true);
+    process_input("let c = blinkfade r 10", cenv, true);
+    process_input("let d = fadein r 10", cenv, true);
+    process_input("let e = fadeout r 10", cenv, true);
+    process_input("let f = wave r 10 10", cenv, true);
+    process_input("let g = wheel r 10 10", cenv, true);
+
+    process_input("split a b c d e f g", cenv, true);
 
     fprintf(stderr, "Done processing input\n");
 
